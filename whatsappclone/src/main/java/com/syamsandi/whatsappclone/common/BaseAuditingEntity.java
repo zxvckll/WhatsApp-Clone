689c192a86b8c1c271ca3949dfd644dda19e5be8
M.Syamsandi <zxvcklh@gmail.com>
2025-03-06 09:54:45 +0700

    set security config

---
+++ whatsappclone/src/main/java/com/syamsandi/whatsappclone/common/BaseAuditingEntity.java
@@ -0,0 +1,4 @@
+package com.syamsandi.whatsappclone.common;
+
+public class BaseAuditingEntity {
+}
--- whatsappclone/src/main/java/com/syamsandi/whatsappclone/security/SecurityConfig.java
+++ whatsappclone/src/main/java/com/syamsandi/whatsappclone/security/SecurityConfig.java
@@ -1,11 +1,21 @@
 package com.syamsandi.whatsappclone.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -33,7 +43,28 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                         )
+                .oauth2ResourceServer(auth ->
+                        auth.jwt(token ->
+                                token.jwtAuthenticationConverter(new KeycloakJwtAuthenticationConverter()))
+                        );
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.ORIGIN,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT,
+                HttpHeaders.AUTHORIZATION
+        ));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
